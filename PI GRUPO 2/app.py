ecddfbede754443a512d4b286932e6252cb94887
ArthurSarturi <seyycookie@gmail.com>
2025-08-21 08:45:13 -0300

    Add files via upload

--- PI GRUPO 2/app.py
+++ PI GRUPO 2/app.py
@@ -9,7 +9,14 @@ def hero():
 @app.route('/home')
 def home():
     return render_template('index.html')
- 
+
+@app.route('/login') 
+def login():
+    return render_template('entrar.html')
+
+@app.route('/forum')
+def forum():
+    return render_template('Forum.html')
 
 if __name__ == '__main__':
     app.run()
