105662f8facdf6b79e2016341edb325d7b3ccb2d
laraappel <laraappel13@gmail.com>
2025-08-28 04:33:33 -0300

    atualização larinha2
    Não sei se esses arquivos são necessários, mas estou dando commit tbm.

--- BOOK HEAL/routes/bookroutes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from controllers import bookcontroller
-
-book_bp = Blueprint("BookBP", __name__, url_prefix="/Books")
-@book_bp.route("/add", methods=["POST"])
-def createBook():
-    return bookcontroller.createBook()
